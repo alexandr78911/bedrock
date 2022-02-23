b2442615edb1c5c181c202a7fa7c65c446a07613
Alexandr <shechenko777777@gmail.com>
2022-02-23 02:21:09 +0200

    ver. 1

--- web/app/themes/Agenturro/resources/views/front-page.blade.php
+++ web/app/themes/Agenturro/resources/views/front-page.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-  <?php if( have_rows('content') ):
-    while( have_rows('content')): the_row();
-      if( get_row_layout() == 'posts' ): ?>
+  @if(have_rows('content'))
+    @while(have_rows('content'))
+      {!! the_row() !!}
+      @if(get_row_layout() == 'posts')
         @include('partials.posts')
-      <?php endif;
-    endwhile;
-  endif;?>
+      @endif
+    @endwhile
+  @endif
 @endsection
